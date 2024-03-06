24fa45df6202b7e0a3df3f61c39ab36ee661bd2d
rn-santos227 <rn_santos227@yahoo.com>
2024-03-06 21:00:40 +0800

    initial commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index() {
+        $tasks = Task::all();
+        return response([
+            'tasks' => $tasks
+        ], 200);
+    }
+
+    public function create(Request $request) {
+        $task = Task::create([
+            'content' => $request->content,
+            'category' => $request->category,
+            'notes' => $request->notes
+        ]);
+
+        return response([
+            'task' => $task
+        ], 200);
+    }
+
+    public function update(Request $request) {
+        $id = $request->id;
+        $task = Task::find($id);
+
+        $task->update([
+            'content' => $request->content,
+            'category' => $request->category,
+            'notes' => $request->notes
+        ]);
+
+        return response([
+            'task' => $task
+        ], 200);
+    }
+}
